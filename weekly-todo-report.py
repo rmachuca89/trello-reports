89b09890572c0563f2934d737e164f2300487ae1
Rodrigo Machuca <rmachuca89@hotmail.com>
2018-12-18 14:39:31 -0600

    Get done open cards name and generate report

--- weekly-todo-report.py
+++ weekly-todo-report.py
@@ -5,6 +5,7 @@ This report generates a summary from all the tasks in the "Done" list
 under the TODO-List Trello Board.
 """
 import os
+import datetime
 from trello import TrelloClient
 
 s_client = TrelloClient(
@@ -19,15 +20,54 @@ o_client = TrelloClient(
     token_secret=os.environ['TRELLO_OATH_SECRET']
 )
 
+
 def get_todo_board():
-    """Return TODO List Board ID."""
+    """Return trello `TODO List` Board object."""
     all_boards = s_client.list_boards()
-    todo_board_id = None
+    todo_board = None
 
     for board in all_boards:
-        print(board.name)
+        if board.name == "TODO List":
+            todo_board = board
+            break
 
-    return todo_board_id
+    return todo_board
+
+
+def get_todo_done_list_cards(trello_board):
+    """Return all open cards in the `Done` list under the `TODO List` board."""
+    done_list = None
+    done_cards = []
+
+    if trello_board:
+        todo_lists = trello_board.list_lists()
+        for trello_list in todo_lists:
+            if trello_list.name == "Done":
+                done_list = trello_list
+                break
+
+    if done_list:
+        done_cards = done_list.list_cards()
+
+    return done_cards
+
 
 if __name__ == "__main__":
-    get_todo_board()
+    print("####################")
+    print("TODO List Weekly Report")
+    print("Date: {}".format(str(datetime.datetime.now())))
+    #  print("Date: {}".format(datetime.datetime.today().strftime('%Y-%m-%d')))
+    print("####################")
+    my_todo_board = get_todo_board()
+    #  print(my_todo_board.id)
+    if not my_todo_board:
+        print("`TODO List` board not found. "
+              "Double check name and that it exists.")
+        exit(1)
+
+    print("--------------------")
+    print("Tasks Done")
+    print("--------------------")
+    my_done_cards = get_todo_done_list_cards(my_todo_board)
+    for card in my_done_cards:
+        print(card.name)
