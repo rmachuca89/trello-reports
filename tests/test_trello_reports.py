c64b0ff8b1d3de6af778f8cf97af356d10d6964c
Rodrigo Machuca <rmachuca89@hotmail.com>
2021-07-23 22:57:26 -0500

    [cli] Creates new CLI main entrypoint with unittest.
    - No actual functionality, just a hello world to define layout.
    - Includes code coverage testing config with pytest-cov

--- tests/test_trello_reports.py
+++ tests/test_trello_reports.py
@@ -1,5 +1,19 @@
-from trello_reports import __version__
+import click.testing
+import pytest
+
+from trello_reports import __version__, cli
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_cli_main_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
