7a4c02eab6a961ae1971859310efe270c7f349c7
Rodrigo Machuca <rmachuca89@hotmail.com>
2021-07-24 20:57:55 -0500

    [build] Adds linters, pre-commit and nox integration

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,32 @@
 import nox
 
+locations = "trello_reports", "tests", "noxfile.py"
+nox.options.sessions = "lint", "tests", "typing"
+
 
 @nox.session(python=["3.9", "3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8", "black")
+    session.run("flake8", *args)
+    session.run("black", "--check", *args)
+
+
+@nox.session
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+@nox.session
+def typing(session):
+    session.install("mypy")
+    session.run("mypy", ".")
